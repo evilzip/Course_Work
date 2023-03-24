6af0ed62a5b06c5e0e96327d4a50cf1d793a1e00
Anton Abdrakhmanov <ode15s@yandex.ru>
2023-03-24 16:33:54 +0300

    Create Input32 method

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int InputInt32(string message) //приглашение ввода с клавиатуры целого числа
+{
+    System.Console.Write(($"{message}: "));
+    int value;
+    bool isCorrectInt32 = int.TryParse(Console.ReadLine(), out value);
+    if (isCorrectInt32)
+    {
+        return value;
+    }
+    System.Console.WriteLine("You entered not a number");
+    Environment.Exit(-1);
+    return 0;
+}
